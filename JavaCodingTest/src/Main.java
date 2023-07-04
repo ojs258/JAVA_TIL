a4e8eaea3fdd8227a57a61784a20890f387efabc
oh jinseok <ojs258@naver.com>
2023-07-04 18:13:36 +0900

    2023-07-04 java condingtest TIL

--- JavaCodingTest/src/Main.java
+++ JavaCodingTest/src/Main.java
@@ -3,23 +3,5 @@
 public class Main {
     public static void main(String[] args) {
 
-
-    }
-}
-
-class Solution55{
-    public int solution(String ineq, String eq, int n, int m) {
-        switch (ineq + eq) {
-            case "<=":
-                return n <= m ? 1 : 0;
-            case ">=":
-                return n >= m ? 1 : 0;
-            case "<!":
-                return n < m ? 1 : 0;
-            case ">!":
-                return n > m ? 1 : 0;
-            default:
-                return 0;
-        }
     }
 }
---
+++ JavaCodingTest/src/easyCondingTest/question27.java
@@ -0,0 +1,29 @@
+package easyCondingTest;
+
+public class question27 {
+    public static void main(String[] args) {
+        new Solution27().solution(0, "wsdawsdassw");
+    }
+}
+
+class Solution27{
+    public int solution(int n, String control) {
+        for (char arrow : control.toCharArray()) {
+            switch (arrow){
+                case 'w':
+                    n += 1;
+                    break;
+                case 's':
+                    n -= 1;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+            }
+        }
+        return n;
+    }
+}
