9ea82e05a2388db128841b930f2efaa21464979c
oh jinseok <ojs258@naver.com>
2023-07-25 17:49:44 +0900

    2023-07-25 코딩테스트

--- JavaCodingTest/src/Hash/retire.java
+++ JavaCodingTest/src/Hash/retire.java
@@ -1,12 +1,38 @@
 package Hash;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class retire {
+    public String solution1(String[] participant, String[] completion) {
+        //LinkedList와 sort로 극한의 최적화로 푼 버전
+        List<String> part = new LinkedList<>(Arrays.asList(participant));
+        List<String> comp = new LinkedList<>(Arrays.asList(completion));
+        Collections.sort(part);
+        Collections.sort(comp);
+        for(String str : comp){
+            part.remove(str);
+        }
+        return part.get(0);
+    }
+
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> players = new HashMap<>();
+        for (String s : participant) {
+            players.put(s, players.getOrDefault(s, 0) + 1);
+        }
+        for (String s : completion){
+            players.put(s,players.get(s)-1);
+        }
+        for(String str : players.keySet()){
+            if(players.get(str).equals(1)){
+                return str;
+            };
+        }
+        return "";
+    }
+
     public static void main(String[] args) {
-        String[] strings = new String[]{"asdf","qwer"};
-        long count = Arrays.stream(strings).filter(s -> s.equals("asdf")).count();
+        String solution = new retire().solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "mislav", "ana"});
+        System.out.println("solution = " + solution);
     }
 }
