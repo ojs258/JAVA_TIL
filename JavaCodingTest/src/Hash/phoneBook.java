c34d0e2071c3e792d314609faee872176c3ee39d
jin5eok5 <ojs258@naver.com>
2023-07-27 22:32:14 +0900

    2023-07-27 코딩테스트

--- JavaCodingTest/src/Hash/phoneBook.java
+++ JavaCodingTest/src/Hash/phoneBook.java
@@ -3,15 +3,16 @@ package Hash;
 import java.util.*;
 
 public class phoneBook {
-    public boolean solution(String[] phone_book) {
-        List<String> pb = new LinkedList<>();
-        Collections.sort(pb);
-        for(String str1 : pb){
-            for(String str2 : pb){
-                if(!str1.equals(str2)){
-                    if(str2.startsWith(str1)){
-                        return false;
-                    }
+    public boolean solution(String[] phoneBook) {
+        //셋 생성 시점에 정렬을 해주는 TreeSet 이용
+        Set<String> pb = new TreeSet<>(List.of(phoneBook));
+
+        // "작은 숫자가 큰 숫자에 접두사인지 확인" 이라는 개념을 버리고
+        // "큰 숫자들의 0~length-1과 같은 숫자가 있는지 확인" 이라는 개념 으로 접근 해야함.
+        for(String str : pb){
+            for(int i = 1; i < str.length(); i++){
+                if(pb.contains(str.substring(0,i))){
+                    return false;
                 }
             }
         }
