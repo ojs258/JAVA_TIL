453b86fd143827b23a20ca722f25070447b15779
jin5eok5 <ojs258@naver.com>
2023-07-30 18:42:29 +0900

    2023-07-30 코딩테스트

--- JavaCodingTest/src/Hash/bestAlbum.java
+++ JavaCodingTest/src/Hash/bestAlbum.java
@@ -1,83 +1,70 @@
 package Hash;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class bestAlbum {
     public int[] solution(String[] genres, int[] plays) {
-        List<Song> songs = new LinkedList<>();
         Map<String, Integer> total = new HashMap<>();
+        Map<String, List<Song>> best = new HashMap<>();
+        List<Integer> answer = new LinkedList<>();
+
+        // totalMap에 각 장르별 재생 수 합계를 저장.
         for(int i = 0; i < genres.length; i++){
-            songs.add(new Song(genres[i],i,plays[i]));
             total.put(genres[i],total.getOrDefault(genres[i],0)+plays[i]);
         }
 
+        // 장르를 리스트로 변환 해서 장르별 재생 수를 기준으로 내림차순 정렬.
+        List<String> sortTotal = new LinkedList<>(total.keySet());
+        sortTotal.sort((o1,o2) -> total.get(o2).compareTo(total.get(o1)));
+
+        // bestMap의 밸류List<Song>타입을 LinkedList로 초기화 해 주면서
+        // 장르 별로 분류 해서 밸류List에 노래 정보(Song 객체)를 저장.
         for(int i = 0; i < genres.length; i++){
-            songs.get(i).setTotal(total.get(songs.get(i).genre));
-        }
-        Comparator<Song> reverse = Comparator.comparing(Song::getPlayCnt).reversed();
-        songs.sort(Comparator.comparing(Song::getTotal).reversed()
-                .thenComparing(reverse)
-                .thenComparing(Song::getPk));
-
-        int[] answer = new int[songs.size() - 1];
-        for(int i = 0; i < songs.size() - 1; i++){
-            answer[i] = songs.get(i).pk;
+            best.putIfAbsent(genres[i],new LinkedList<>());
+            best.get(genres[i]).add(new Song(plays[i], i));
         }
 
-        Map.Entry<Integer, Integer> integerIntegerEntry = Map.entry(1,1);
-        List<Map.Entry<Integer, Integer>> abc = new LinkedList<>();
-
-        abc.add(Map.entry(1234,18899));
-        abc.add(Map.entry(13245,45645));
-        abc.add(Map.entry(12,121234));
-        abc.add(Map.entry(1123451,12123));
-        abc.sort((o1, o2) -> o2.getKey().compareTo(o1.getKey()));
-
-        for (Map.Entry<Integer, Integer> integerEntry : abc) {
-            System.out.println("integerEntry.getKey() = " + integerEntry.getKey());
+        // 장르 별로 분류 되어있는 노래 정보를 객체 안에 오버 라이딩 해놓은
+        //  compareTo 메서드를 이용해 재생 수의 내림차순으로 정렬.
+        for(List<Song> songs : best.values()){
+            Collections.sort(songs);
         }
 
-        List<Integer> ints = Arrays.stream(plays).boxed().collect(Collectors.toList();
-        // 장르별 재생수 desc
-        // 노래별 재생수 desc
-        // 노래 고유번호 asc
-        return answer;
+        // 정답 리스트 answer에 subList()메서드를 이용해 각 장르병 TOP2를 골라서 저장 한다.
+        // 만약 하나의 장르에 해당 하는 노래가 하나 밖에 없다면 songs.size()가 1이 되므로
+        // Math.min()메서드에 의해 TOP1만 answerList에 저장 된다.
+        for(String str : sortTotal){
+            List<Song> songs = best.get(str);
+            List<Song> subSongs = songs.subList(0,Math.min(2,songs.size()));
+            for(Song song : subSongs){
+                answer.add(song.index);
+            }
+        }
+        // 리턴 타입에 맞게 변환 해서 정답을 리턴
+        return answer.stream().mapToInt(n -> n).toArray();
     }
 
+    static class Song implements Comparable<Song> {
+        int play;
+        int index;
+
+        public Song(int play, int index){
+            this.play = play;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Song other){
+
+            // 노래별 재생 수 desc 정렬
+            return other.play - this.play;
+        }
+
+    }
     public static void main(String[] args) {
         int[] solution = new bestAlbum().solution(new String[]{"classic", "pop", "classic", "classic", "pop"}, new int[]{500, 600, 150, 800, 2500});
         for (int i : solution) {
             System.out.println("i = " + i);
         }
-    }
-}
-
-class Song {
-    String genre;
-    int pk;
-    int playCnt;
-    int total;
-
-    public Song(String genre, int pk, int playCnt){
-        this.genre = genre;
-        this.pk = pk;
-        this.playCnt = playCnt;
-    }
-
-    public void setTotal(int total){
-        this.total = total;
-    }
-
-    public int getTotal(){
-        return total;
-    }
-
-    public int getPlayCnt(){
-        return playCnt;
-    }
-
-    public int getPk(){
-        return pk;
     }
 }
