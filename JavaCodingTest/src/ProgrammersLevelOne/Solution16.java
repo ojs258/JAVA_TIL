a7c23619673ca1caeafeef9cc04f4af3fc6645fd
jin5eok5 <ojs258@naver.com>
2023-08-06 23:56:05 +0900

    2023-08-06 코딩테스트

---
+++ JavaCodingTest/src/ProgrammersLevelOne/Solution16.java
@@ -0,0 +1,14 @@
+package ProgrammersLevelOne;
+
+import java.util.Arrays;
+
+public class Solution16 {
+    public int solution(int[] ingredient){
+
+        int[] arr = new int[]{2,1,1,2,3,1,2,3,1};
+        int[] burger = new int[]{1,2,3,1};
+
+        Arrays.
+        return 0;
+    }
+}
