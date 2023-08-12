ea578cebe2ab7406411d4da8947250e25698bbff
jin5eok5 <ojs258@naver.com>
2023-08-12 22:17:12 +0900

    2023-08-12 코딩테스트

--- JavaCodingTest/src/StackAndQueue/PriorityQueue.java
+++ JavaCodingTest/src/StackAndQueue/PriorityQueue.java
@@ -15,7 +15,33 @@ public class PriorityQueue {
         return 1;
 
     }
-    // index
-    // priority 내림차순
-    // rank
+    // 현재 index와 값이 담긴 객치 priority가를 리스트에 저장
+    // priority가 내림차순으로 정렬될 때까지 왼쪽 쉬프트 연산.
+    // 내림차순으로 정렬되고 나면 그 때의 인덱스가 rank
+    // 객체의 index가 location과 일치하는 요소의 인덱스를 리턴.
+    class priority{
+        int index;
+        int number;
+
+        public priority(int index, int number) {
+            this.index = index;
+            this.number = number;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+    }
 }
