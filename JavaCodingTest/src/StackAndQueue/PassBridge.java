f9bbe7095aa48b94bbf93e9679f8b93e022026fb
jin5eok5 <ojs258@naver.com>
2023-08-15 21:57:41 +0900

    2023-08-15 코딩테스트

--- JavaCodingTest/src/StackAndQueue/PassBridge.java
+++ JavaCodingTest/src/StackAndQueue/PassBridge.java
@@ -10,7 +10,7 @@ public class PassBridge {
         // 마지막엔 대기하는 자동차 목록, 다리는 건너는 자동차 목록이 전부 0이면 리턴.
         int answer = 0;
         boolean cond = false;
-        List<Car> bridge = new ArrayList<>();
+        List<Truck> bridge = new ArrayList<>();
         Queue<Integer> restCar = new LinkedList<>(Arrays.asList(Arrays.stream(truck_weights)
                 .boxed()
                 .toArray(Integer[]::new)));
@@ -18,36 +18,53 @@ public class PassBridge {
         while(!restCar.isEmpty() || !bridge.isEmpty()){
             // 다리의 조건에따른 자동차를 다리위로 올려보내는 로직
             if(!restCar.isEmpty()){
-                boolean isWeight = weight - bridgeWeight > restCar.peek();
+                // 다리의 무게 초과를 체크
+                boolean isWeight = weight - bridgeWeight >= restCar.peek();
+                // 다리의 길이 초과를 체크
                 boolean isLength = bridge_length > bridge.size();
                 if(isWeight && isLength) {
-                    int carWeight = restCar.poll();
-                    bridgeWeight += carWeight;
-                    bridge.add(new Car(carWeight, bridge_length));
+                    int truckWeight = restCar.poll();
+                    bridgeWeight += truckWeight;
+                    bridge.add(new Truck(truckWeight, bridge_length));
                 }
             }
             // 초 단위 시뮬 카운트 answer;
             answer++;
+
             // 1초가 흘렀으므로 다리위의 차들을 1칸씩 이동시키고
             // 남은 이동거리가 0인 차들을 다리에서 내려보내는 로직
             for(int i = 0; i < bridge.size(); i++){
-                bridge.get(i).moveCnt--;
-                if(bridge.get(i).moveCnt == 0){
+                bridge.get(i).move();
+                if(bridge.get(i).done()){
                     bridgeWeight -= bridge.get(i).weight;
                     bridge.remove(bridge.get(i));
+                    i--;
                 }
             }
+
         }
-        return answer;
+        // 마지막 차량이 다리에서 내려오는 시간 1초를 더해서 answer를 리턴.
+        return answer + 1;}
+
+    public static void main(String[] args) {
+        int solution = new PassBridge().solution(100,100,new int[]{10,10,10,10,10,10,10,10,10,10});
+        System.out.println("solution = " + solution);
     }
 }
 
-class Car {
+class Truck {
     int weight;
     int moveCnt;
 
-    public Car(int weight, int moveCnt) {
+    public Truck(int weight, int moveCnt) {
         this.weight = weight;
         this.moveCnt = moveCnt;
+    }
+
+    public void move() {
+        moveCnt--;
+    }
+    public boolean done(){
+        return moveCnt == 0;
     }
 }
