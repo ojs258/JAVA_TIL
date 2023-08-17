cb0b3f3789f308856b0a7df7faf7b5cead4376f6
oh jinseok <ojs258@naver.com>
2023-08-17 17:50:53 +0900

    2023-08-17 코딩테스트

--- JavaCodingTest/src/ProgrammersHighScoreKit/StackAndQueue/StockPrice.java
+++ JavaCodingTest/src/ProgrammersHighScoreKit/StackAndQueue/StockPrice.java
@@ -13,7 +13,7 @@ public class StockPrice {
             if(prices[i] < prices[i-1]) downPrices.push(i);
         }
         downPrices.push(max);
-
+        downPrices.peek();
         while(!downPrices.isEmpty()) {
             int downIndex = downPrices.pop();
             for(int i = 0; i < downIndex; i++) {
@@ -25,5 +25,44 @@ public class StockPrice {
             }
         }
         return answer;
+    }
+
+    public int[] solution1(int[] prices) {
+        List<Integer> priceList = new ArrayList<>();
+        Stack<Integer> downPrices = new Stack<>();
+        int max = prices.length - 1;
+        int[] answer = new int[prices.length];
+
+        for(int i = 1; i < prices.length; i++){
+            priceList.add(i-1);
+            if(prices[i] < prices[i-1]) downPrices.push(i);
+        }
+        downPrices.push(max);
+
+        List<Integer> temp = new LinkedList<>();
+        while(!downPrices.isEmpty()) {
+            int downIndex = downPrices.pop();
+            for(int n : priceList) {
+                if(prices[n] > prices[downIndex]) {
+                    answer[n] = downIndex - n;
+                    temp.add(n);
+                } else if(answer[n] == 0) {
+                    answer[n] = max - n;
+                }
+            }
+
+            for(int n : temp) {
+                priceList.remove(n);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new StockPrice().solution1(new int[]{1, 2, 3, 2, 3});
+
+        for (int anInt : ints) {
+            System.out.println("anInt = " + anInt);
+        }
     }
 }
