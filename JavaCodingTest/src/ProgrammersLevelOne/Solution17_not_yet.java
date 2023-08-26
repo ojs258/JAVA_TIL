eebf6246e4961bfea7718bf28cba23e755eb6ad4
jin5eok5 <ojs258@naver.com>
2023-08-26 23:08:53 +0900

    2023-08-26 코딩테스트

--- JavaCodingTest/src/ProgrammersLevelOne/Solution17_not_yet.java
+++ JavaCodingTest/src/ProgrammersLevelOne/Solution17_not_yet.java
@@ -1,32 +1,20 @@
 package ProgrammersLevelOne;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Solution17_not_yet {
 
     public int solution(int[] ingredient) {
-        List<Integer> ingredientList = Arrays.stream(ingredient).boxed().collect(Collectors.toList());
         Stack<Integer> stk = new Stack<>();
+        Queue<Integer> q = new LinkedList<>();
         int answer = 0;
+        int[] burger = new int[]{1,2,3,1};
 
+        int cnt = 0;
+        for(int i = 0; i < ingredient.length; i++) {
 
-        for(int i = 0; i < ingredient.length - 3; i++) {
-            String burger = "";
-            stk.push(ingredient[i]);
-            burger += String.valueOf(stk.peek());
-            for(int j = i + 1 ; j < i + 4; j++) {
-                burger += String.valueOf(ingredient[j]);
-            }
-            if(burger.equals("1231")) {
-                answer++;
-                stk.pop();
-                i += 4;
-            }
         }
-
         return answer;
     }
 }
