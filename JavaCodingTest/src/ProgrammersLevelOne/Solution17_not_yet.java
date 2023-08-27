5dba75b9df5ccdd1a01bad46508b06fe14ec6c4a
jin5eok5 <ojs258@naver.com>
2023-08-27 22:52:57 +0900

    2023-08-27

--- JavaCodingTest/src/ProgrammersLevelOne/Solution17_not_yet.java
+++ JavaCodingTest/src/ProgrammersLevelOne/Solution17_not_yet.java
@@ -4,17 +4,36 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Solution17_not_yet {
-
     public int solution(int[] ingredient) {
-        Stack<Integer> stk = new Stack<>();
-        Queue<Integer> q = new LinkedList<>();
+        List<Integer> list = new LinkedList<>(Arrays.asList(Arrays.stream(ingredient)
+                .boxed()
+                .toArray(Integer[]::new)));
         int answer = 0;
-        int[] burger = new int[]{1,2,3,1};
-
         int cnt = 0;
-        for(int i = 0; i < ingredient.length; i++) {
-
+        String burger = "1231";
+        for(int i = 0; i < list.size(); i++) {
+            String newBurger = "";
+            if(i == list.size() - 3) {
+                return answer;
+            }
+            for(int j = i; j < 4 + i; j++) {
+                newBurger += list.get(j);
+            }
+            if(burger.equals(newBurger)) {
+                answer++;
+                for(int k = i; k < 4 + i; k++) {
+                    list.remove(i);
+                }
+                i -= 2;
+            }
+            if(list.size() < 4){
+                return answer;
+            }
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        new Solution17_not_yet().solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1});
     }
 }
