d832f647d51ed76658134cef1a941eea8fb0d46c
oh jinseok <ojs258@naver.com>
2023-08-29 16:01:16 +0900

    2023-08-29 코딩테스트

--- JavaCodingTest/src/ProgrammersHighScoreKit/ExhaustiveSearch/minimumSquare.java
+++ JavaCodingTest/src/ProgrammersHighScoreKit/ExhaustiveSearch/minimumSquare.java
@@ -1,11 +1,15 @@
 package ProgrammersHighScoreKit.ExhaustiveSearch;
 
+import java.util.Arrays;
+
 public class minimumSquare {
     class Solution {
         public int solution(int[][] sizes) {
             int[] width = new int[sizes.length];
             int[] height = new int[sizes.length];
-            //하.. 인생이 너무 어렵다...
+
+            int w = Arrays.stream(width).max().getAsInt();
+            int h = Arrays.stream(height).max().getAsInt();
 
             return 0;
         }
