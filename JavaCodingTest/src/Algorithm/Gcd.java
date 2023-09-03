46a21abc95c5ec1ca173329ba8134460353464a6
jin5eok5 <bejin5eok5@gmail.com>
2023-09-03 22:41:03 +0900

    2023-09-03 코딩테스트

---
+++ JavaCodingTest/src/Algorithm/Gcd.java
@@ -0,0 +1,26 @@
+package Algorithm;
+
+public class Gcd {
+    // 기본적인 방법
+    public int gcd1(int a, int b) {
+        int size = Math.min(a,b);
+        for(int i = 1; i < size; i++) {
+
+        }
+    }
+
+// 유클리드 호제법 - 반복문을 이용한 방법
+    public int gcd2(int a, int b) {
+        while(a%b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return b;
+    }
+// 유클리드 호제법 - 재귀를 이용한 방법
+    public int gcd3(int a, int b) {
+        if (a % b == 0) return b;
+        return gcd3(b, a%b);
+    }
+}
---
+++ JavaCodingTest/src/Algorithm/Lcm.java
@@ -0,0 +1,13 @@
+package Algorithm;
+
+public class Lcm {
+    public int gcd(int a, int b) {
+        if (a % b == 0) return b;
+        return gcd(b, a%b);
+    }
+
+    public int lcm(int a, int b) {
+        return (a * b) / gcd(b, a%b);
+        // 자바는 int형끼리 나누면 자동으로 몫 나눗셈 결과가 나온다.
+    }
+}
