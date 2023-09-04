8c38fb8619614f59e311a4f52839aad3d44d4190
oh jinseok <ojs258@naver.com>
2023-09-04 18:00:41 +0900

    2023-09-04 최대 공약수 최소 공배수 유클리드 알고리즘 정리

--- JavaCodingTest/src/Algorithm/Gcd.java
+++ JavaCodingTest/src/Algorithm/Gcd.java
@@ -4,9 +4,10 @@ public class Gcd {
     // 기본적인 방법
     public int gcd1(int a, int b) {
         int size = Math.min(a,b);
-        for(int i = 1; i < size; i++) {
-
+        for(int i = size; i > 0; i--) {
+            if(a % i == 0 && b % i == 0) return i;
         }
+        return 1;
     }
 
 // 유클리드 호제법 - 반복문을 이용한 방법
